0acbd7f1e9e62629a5f1b2bbcd5d707496741926
lucindasouchet <lucinda@strikepay.com>
2021-11-26 01:43:52 +0100

    style: update app and initial component style

--- src/components/initial.js
+++ src/components/initial.js
@@ -1,8 +1,20 @@
+// Initial component
 const Initial = () => {
 
     return(
         <div>
-            initial works
+            <h2 className="text-center">Ask a question</h2>
+            <input 
+                name="question"
+                type="text"
+                className="form-control mt-4"
+            />
+
+            <button
+                className="btn btn-danger mt-4 mx-auto d-block"
+            >
+                Next
+            </button>
         </div>
     )
 }
